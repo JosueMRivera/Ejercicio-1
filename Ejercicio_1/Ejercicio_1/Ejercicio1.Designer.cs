2a129bd8692a63e208f6ee8c2ecb8d15b0809144
Josue Rivera <jm.rivera@unah.hn>
2023-02-03 22:06:10 -0600

    Segundo Commit
    creacion de 2 funciones para calcular el numero par o impar y positivo o negativo

--- Ejercicio_1/Ejercicio_1/Ejercicio1.Designer.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ejercicio_1
 {
-    partial class Form1
+    partial class Ejercicio1
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -30,12 +30,66 @@ namespace Ejercicio_1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numerotxt = new System.Windows.Forms.TextBox();
+            this.ejecutarbt = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un numero:";
+            // 
+            // numerotxt
+            // 
+            this.numerotxt.Location = new System.Drawing.Point(141, 34);
+            this.numerotxt.Name = "numerotxt";
+            this.numerotxt.Size = new System.Drawing.Size(121, 20);
+            this.numerotxt.TabIndex = 1;
+            // 
+            // ejecutarbt
+            // 
+            this.ejecutarbt.Location = new System.Drawing.Point(141, 60);
+            this.ejecutarbt.Name = "ejecutarbt";
+            this.ejecutarbt.Size = new System.Drawing.Size(121, 28);
+            this.ejecutarbt.TabIndex = 2;
+            this.ejecutarbt.Text = "Ejecutar";
+            this.ejecutarbt.UseVisualStyleBackColor = true;
+            this.ejecutarbt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(391, 231);
+            this.Controls.Add(this.ejecutarbt);
+            this.Controls.Add(this.numerotxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Ejercicio1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numerotxt;
+        private System.Windows.Forms.Button ejecutarbt;
+        private System.Windows.Forms.ErrorProvider errorProvider;
     }
 }
 
--- Ejercicio_1/Ejercicio_1/Ejercicio1.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio1.cs
@@ -10,11 +10,62 @@ using System.Windows.Forms;
 
 namespace Ejercicio_1
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio1 : Form
     {
-        public Form1()
+        public Ejercicio1()
         {
             InitializeComponent();
         }
+
+        private void Ejercicio1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numerotxt.Text == "")
+            {
+                errorProvider.SetError(numerotxt, "Ingrese un valor");
+                return;
+            }
+            errorProvider.Clear();
+            string r1 = paroimpar(Convert.ToInt32(numerotxt.Text));
+            string r2 = positivoonegativo(Convert.ToInt32(numerotxt.Text));
+            MessageBox.Show("El numero ingresado es " + r1 + r2, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string paroimpar(int n1)
+        {
+
+            if ((n1%2)== 0)
+            {
+                return "par y ";
+            }
+            else
+            {
+                return "impar y ";
+            }
+
+        }
+
+        private string positivoonegativo(int n1)
+        {
+
+            if (n1 >= 1)
+            {
+                return "positivo";
+            }
+            else if (n1 <= -1)
+            {
+                return "negativo";
+            }
+
+            return "";
+
+        }
+
+
+
     }
 }
